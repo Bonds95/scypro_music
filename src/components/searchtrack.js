7ba5f0f3f8a7295625fe1244614087dbe9614a2a
Bonds95 <sergeybondarenko95@yandex.ru>
2023-10-06 14:26:08 +0200

    вынес компонент поиска трека

---
+++ src/components/searchtrack.js
@@ -0,0 +1,17 @@
+import "./searchtrack.css"
+
+export default function SearchTrack() {
+    return (
+        <div className="centerblock__search search">
+          <svg className="search__svg">
+            <use xlinkHref="img/icon/sprite.svg#icon-search" />
+          </svg>
+          <input
+            className="search__text"
+            type="search"
+            placeholder="Поиск"
+            name="search"
+          />
+        </div>
+    )
+}
--- src/components/tracklist.js
+++ src/components/tracklist.js
@@ -1,19 +1,10 @@
 import "./tracklist.css"
+import SearchTrack from "./searchtrack"
 
 export default function Tracklist() {
     return (
         <div className="main__centerblock centerblock">
-        <div className="centerblock__search search">
-          <svg className="search__svg">
-            <use xlinkHref="img/icon/sprite.svg#icon-search" />
-          </svg>
-          <input
-            className="search__text"
-            type="search"
-            placeholder="Поиск"
-            name="search"
-          />
-        </div>
+        <SearchTrack/>
         <h2 className="centerblock__h2">Треки</h2>
         <div className="centerblock__filter filter">
           <div className="filter__title">Искать по:</div>
