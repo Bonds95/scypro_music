cad33dacbd2ed2428cab8a32f75a57276901bc51
Bonds95 <sergeybondarenko95@yandex.ru>
2023-10-11 16:37:41 +0200

    сделал 1 задачу с бургером

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,18 +1,26 @@
+import { useState } from "react";
+
 import "./navbar.css"
 import NavMenu from "./navmenu"
 
 export default function NavBar() {
+
+  const [vision, setVision] = useState(true);
+
+  function handleClick() {
+    setVision(!vision)
+  }
     return (
         <nav className="main__nav nav">
         <div className="nav__logo logo">
           <img className="logo__image" src="img/logo.png" alt="logo" />
         </div>
-        <div className="nav__burger burger">
+        <div onClick={handleClick} className="nav__burger burger">
           <span className="burger__line" />
           <span className="burger__line" />
           <span className="burger__line" />
         </div>
-        <NavMenu/>
+        {vision && <NavMenu/>}
       </nav>
     )
 }
