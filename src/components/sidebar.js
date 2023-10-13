0b77705f7fdb23b680989790286b0ac494c2ded7
Bonds95 <sergeybondarenko95@yandex.ru>
2023-10-13 15:14:09 +0200

    отредактировал код заменив условные ветвление на условный опретор ?

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -6,31 +6,20 @@ import LoadPlaylistItem from "./LoadPlaylistItem"
 
 export default function Sidebar({load}) {
 
-if (load === false) {
-  return (
-        <div className="main__sidebar sidebar">
-        <ExitPanel/>
-        <div className="sidebar__block">
-          <div className="sidebar__list">
-           {loadplaylistImg.map(img => 
-            <LoadPlaylistItem img={img} key={img.id}/>)}
-          </div>
-        </div>
-      </div>
-    )
-}
-else {
+
+
   return (
     <div className="main__sidebar sidebar">
     <ExitPanel/>
     <div className="sidebar__block">
       <div className="sidebar__list">
-       {playlistImg.map(img => 
-        <PlaylistItem img={img} key={img.id}/>)}
+       { load ? playlistImg.map(img => 
+        <PlaylistItem img={img} key={img.id}/>) : loadplaylistImg.map(img => 
+          <LoadPlaylistItem img={img} key={img.id}/>) }
       </div>
     </div>
   </div>
 )
-}
+
     
 }
--- src/components/trackcontainer.js
+++ src/components/trackcontainer.js
@@ -4,45 +4,25 @@ import { trackInfo } from "./arrows";
 import LoadTrackItem from "./LoadTrackItem";
 
 export default function TrackContainer({ load }) {
-  if (load === false) {
-    return (
-      <div className="centerblock__content">
-        <div className="content__title playlist-title">
-          <div className="playlist-title__col col01">Трек</div>
-          <div className="playlist-title__col col02">ИСПОЛНИТЕЛЬ</div>
-          <div className="playlist-title__col col03">АЛЬБОМ</div>
-          <div className="playlist-title__col col04">
-            <svg className="playlist-title__svg" alt="time">
-              <use xlinkHref="img/icon/sprite.svg#icon-watch" />
-            </svg>
-          </div>
-        </div>
-        <div className="content__playlist playlist">
-          {trackInfo.map((track) => (
-            <LoadTrackItem track={track} key={track.id} />
-          ))}
+  return (
+    <div className="centerblock__content">
+      <div className="content__title playlist-title">
+        <div className="playlist-title__col col01">Трек</div>
+        <div className="playlist-title__col col02">ИСПОЛНИТЕЛЬ</div>
+        <div className="playlist-title__col col03">АЛЬБОМ</div>
+        <div className="playlist-title__col col04">
+          <svg className="playlist-title__svg" alt="time">
+            <use xlinkHref="img/icon/sprite.svg#icon-watch" />
+          </svg>
         </div>
       </div>
-    );
-  } else {
-    return (
-      <div className="centerblock__content">
-        <div className="content__title playlist-title">
-          <div className="playlist-title__col col01">Трек</div>
-          <div className="playlist-title__col col02">ИСПОЛНИТЕЛЬ</div>
-          <div className="playlist-title__col col03">АЛЬБОМ</div>
-          <div className="playlist-title__col col04">
-            <svg className="playlist-title__svg" alt="time">
-              <use xlinkHref="img/icon/sprite.svg#icon-watch" />
-            </svg>
-          </div>
-        </div>
-        <div className="content__playlist playlist">
-          {trackInfo.map((track) => (
-            <TrackItem track={track} key={track.id} />
-          ))}
-        </div>
+      <div className="content__playlist playlist">
+        {load
+          ? trackInfo.map((track) => <TrackItem track={track} key={track.id} />)
+          : trackInfo.map((track) => (
+              <LoadTrackItem track={track} key={track.id} />
+            ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
