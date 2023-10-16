d51db792a7a7dae0f6af8d94e82f8f47ed7c00a1
Bonds95 <sergeybondarenko95@yandex.ru>
2023-10-16 13:11:14 +0200

    неудачная попытка выполнить 3 задание, не понимаю как сделать общее состояние для трех всплывающих окон

--- src/components/TrackFilter.js
+++ src/components/TrackFilter.js
@@ -1,24 +1,37 @@
-import FilterWindow from "./FilterWindow"
+import FilterWindowAuthor from "./FilterWindowAuthor";
+import FilterWindowGenre from "./FilterWindowGenre";
 import { useState } from "react";
 
 export default function TrackFilter() {
-
-    const [filter, setFilter] = useState(false);
+  const [filter, setFilter] = useState(false);
 
   function handleClick() {
-    setFilter(!filter)
+    setFilter(!filter);
   }
-    return(
-        <div className="centerblock__filter filter">
-            {filter && <FilterWindow />}
-          <div className="filter__title">Искать по:</div>
-          <div onClick={handleClick} className="filter__button button-author _btn-text">
-            исполнителю
-          </div>
-          <div className="filter__button button-year _btn-text">
-            году выпуска
-          </div>
-          <div className="filter__button button-genre _btn-text">жанру</div>
-        </div>
-    )
+  const [filter2, setFilter2] = useState(false);
+
+  function handleClick2() {
+    setFilter2(!filter2);
+  }
+  return (
+    <div className="centerblock__filter filter">
+      {filter && <FilterWindowAuthor />}
+      {filter2 && <FilterWindowGenre />}
+
+      <div className="filter__title">Искать по:</div>
+      <div
+        onClick={handleClick}
+        className="filter__button button-author _btn-text"
+      >
+        исполнителю
+      </div>
+      <div
+        onClick={handleClick2}
+        className="filter__button button-year _btn-text"
+      >
+        году выпуска
+      </div>
+      <div className="filter__button button-genre _btn-text">жанру</div>
+    </div>
+  );
 }
