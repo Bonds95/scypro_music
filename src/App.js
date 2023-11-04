b4e37f200daa08e5949097d8ef0493dabf5537b2
Bonds95 <sergeybondarenko95@yandex.ru>
2023-11-04 13:08:53 +0100

    вынес данные из app.js в новый компонент MainPage

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import "./App.css";
-import Player from "./components/Player/player";
-import Sidebar from "./components/SideBar/sidebar";
-import Mainblock from "./components/MainBlock/mainblock";
-import NavBar from "./components/NavBar/NavBar";
-import { useState, useEffect } from "react";
 import { GlobalStyle } from "./GlobalStyle";
-import * as S from "./App.styles";
+import { MainPage } from "./pages/main/MainPage";
 
 function App() {
-  const [load, setLoad] = useState(false);
-  useEffect(() => {
-    setTimeout(() => {
-      setLoad(!load)
-    }, 5000)
-  }, []);
   
+
   return (
     <div>
       <GlobalStyle />
-      <S.Wrapper>
-        <S.Container>
-          <S.Main>
-            <NavBar />
-            <Mainblock load={load} />
-            <Sidebar load={load} />
-          </S.Main>
-          <Player load={load} />
-          <S.Footer />
-        </S.Container>
-      </S.Wrapper>
+      <MainPage></MainPage>
     </div>
   );
 }
---
+++ src/pages/main/MainPage.jsx
@@ -0,0 +1,33 @@
+import Player from "../../components/Player/player";
+import Sidebar from "../../components/SideBar/sidebar";
+import Mainblock from "../../components/MainBlock/mainblock";
+import NavBar from "../../components/NavBar/NavBar";
+import { useState, useEffect } from "react";
+import * as S from "./MainPage.styles";
+
+export function MainPage() {
+  const [load, setLoad] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoad(!load)
+    }, 5000)
+  }, []);
+  
+  return (
+    <div>
+      <S.Wrapper>
+        <S.Container>
+          <S.Main>
+            <NavBar />
+            <Mainblock load={load} />
+            <Sidebar load={load} />
+          </S.Main>
+          <Player load={load} />
+          <S.Footer />
+        </S.Container>
+      </S.Wrapper>
+    </div>
+  );
+}
+
+
