b09b178363d8e5558b2dd9cf08a044a985070210
Bonds95 <sergeybondarenko95@yandex.ru>
2023-11-24 12:05:03 +0100

    l

--- src/components/Player/player.jsx
+++ src/components/Player/player.jsx
@@ -1,79 +1,104 @@
-
 import PlayerTrackInfo from "./PlayerTrackInfo";
 import LoadPlayerTrackInfo from "./LoadPlayerTrackInfo";
-import * as S from "./player.styles"
+import * as S from "./player.styles";
+// import { useRef, useState } from "react";
 
-export default function Player({currentTrack, load }) {
+export default function Player({ currentTrack, load }) {
+  // const [isPlaying, setIsPlaying] = useState(false);
+  // const audioRef = useRef(null);
+
+  // const handleStart = () => {
+  //   audioRef.current.play();
+  //   setIsPlaying(true);
+  // };
+
+  // const handleStop = () => {
+  //   audioRef.current.pause();
+  //   setIsPlaying(false);
+  // };
+
+  // const togglePlay = isPlaying ? handleStop : handleStart;
+
   return (
-    <S.Bar>
-      <S.BarContent>
-        <S.BarPlayerProgress />
-        <S.BarPlayerBlock>
-          <S.BarPlayer>
-            <S.PlayerControls>
-              <S.PlayerBtnPrev>
-                <S.PlayerBtnPrevSvg alt="prev">
-                  <use xlinkHref="img/icon/sprite.svg#icon-prev" />
-                </S.PlayerBtnPrevSvg>
-              </S.PlayerBtnPrev>
-              <S.PlayerBtnPlay>
-                <S.PlayerBtnPlaySvg alt="play">
-                  <use xlinkHref="img/icon/sprite.svg#icon-play" />
-                </S.PlayerBtnPlaySvg>
-              </S.PlayerBtnPlay>
-              <S.PlayBtnNext>
-                <S.PlayerBtnNextSvg alt="next">
-                  <use xlinkHref="img/icon/sprite.svg#icon-next" />
-                </S.PlayerBtnNextSvg>
-              </S.PlayBtnNext>
-              <S.PlayerBtnRepeat className="_btn-icon">
-                <S.PlayerBtnRepeatSvg alt="repeat">
-                  <use xlinkHref="img/icon/sprite.svg#icon-repeat" />
-                </S.PlayerBtnRepeatSvg>
-              </S.PlayerBtnRepeat>
-              <S.PlayerBtnShuffle className="_btn-icon">
-                <S.PlayerBtnShuffleSvg alt="shuffle">
-                  <use xlinkHref="img/icon/sprite.svg#icon-shuffle" />
-                </S.PlayerBtnShuffleSvg>
-              </S.PlayerBtnShuffle>
-            </S.PlayerControls>
-            <S.PlayerTrackPlay>
-              {load ? <PlayerTrackInfo currentTrack={currentTrack}/> : <LoadPlayerTrackInfo />}
-              <S.PlayerTrackPlayLikeDis>
-                <S.PlayerTrackPlayLike className="_btn-icon">
-                  <S.PlayerTrackPlayLikeSvg alt="like">
-                    <use xlinkHref="img/icon/sprite.svg#icon-like" />
-                  </S.PlayerTrackPlayLikeSvg>
-                </S.PlayerTrackPlayLike>
-                <S.PlayerTrackPlayDislike className="_btn-icon">
-                  <S.PlayerTrackPlayDislikeSvg
-                    className="track-play__dislike-svg"
-                    alt="dislike"
-                  >
-                    <use xlinkHref="img/icon/sprite.svg#icon-dislike" />
-                  </S.PlayerTrackPlayDislikeSvg>
-                </S.PlayerTrackPlayDislike>
-              </S.PlayerTrackPlayLikeDis>
-            </S.PlayerTrackPlay>
-          </S.BarPlayer>
-          <S.BarVolumeBlock>
-            <S.BarVolumeContent>
-              <S.BarVolumeImage>
-                <S.BarVolumeImageSvg alt="volume">
-                  <use xlinkHref="img/icon/sprite.svg#icon-volume" />
-                </S.BarVolumeImageSvg>
-              </S.BarVolumeImage>
-              <S.BarVolumeProgress className="_btn">
-                <S.BarVolumeProgressLine
-                  className="_btn"
-                  type="range"
-                  name="range"
-                />
-              </S.BarVolumeProgress>
-            </S.BarVolumeContent>
-          </S.BarVolumeBlock>
-        </S.BarPlayerBlock>
-      </S.BarContent>
-    </S.Bar>
+    <>
+      {/* <audio controls ref={audioRef}>
+        <source src={currentTrack.track_file} type="audio/mpeg" />
+      </audio> */}
+
+      <S.Bar>
+        <S.BarContent>
+          <S.BarPlayerProgress />
+          <S.BarPlayerBlock>
+            <S.BarPlayer>
+              <S.PlayerControls>
+                <S.PlayerBtnPrev>
+                  <S.PlayerBtnPrevSvg alt="prev">
+                    <use xlinkHref="img/icon/sprite.svg#icon-prev" />
+                  </S.PlayerBtnPrevSvg>
+                </S.PlayerBtnPrev>
+                <S.PlayerBtnPlay >
+                  <S.PlayerBtnPlaySvg alt="play">
+                    <use xlinkHref="img/icon/sprite.svg#icon-play" />
+                  </S.PlayerBtnPlaySvg>
+                </S.PlayerBtnPlay>
+                <S.PlayBtnNext>
+                  <S.PlayerBtnNextSvg alt="next">
+                    <use xlinkHref="img/icon/sprite.svg#icon-next" />
+                  </S.PlayerBtnNextSvg>
+                </S.PlayBtnNext>
+                <S.PlayerBtnRepeat className="_btn-icon">
+                  <S.PlayerBtnRepeatSvg alt="repeat">
+                    <use xlinkHref="img/icon/sprite.svg#icon-repeat" />
+                  </S.PlayerBtnRepeatSvg>
+                </S.PlayerBtnRepeat>
+                <S.PlayerBtnShuffle className="_btn-icon">
+                  <S.PlayerBtnShuffleSvg alt="shuffle">
+                    <use xlinkHref="img/icon/sprite.svg#icon-shuffle" />
+                  </S.PlayerBtnShuffleSvg>
+                </S.PlayerBtnShuffle>
+              </S.PlayerControls>
+              <S.PlayerTrackPlay>
+                {load ? (
+                  <PlayerTrackInfo currentTrack={currentTrack} />
+                ) : (
+                  <LoadPlayerTrackInfo />
+                )}
+                <S.PlayerTrackPlayLikeDis>
+                  <S.PlayerTrackPlayLike className="_btn-icon">
+                    <S.PlayerTrackPlayLikeSvg alt="like">
+                      <use xlinkHref="img/icon/sprite.svg#icon-like" />
+                    </S.PlayerTrackPlayLikeSvg>
+                  </S.PlayerTrackPlayLike>
+                  <S.PlayerTrackPlayDislike className="_btn-icon">
+                    <S.PlayerTrackPlayDislikeSvg
+                      className="track-play__dislike-svg"
+                      alt="dislike"
+                    >
+                      <use xlinkHref="img/icon/sprite.svg#icon-dislike" />
+                    </S.PlayerTrackPlayDislikeSvg>
+                  </S.PlayerTrackPlayDislike>
+                </S.PlayerTrackPlayLikeDis>
+              </S.PlayerTrackPlay>
+            </S.BarPlayer>
+            <S.BarVolumeBlock>
+              <S.BarVolumeContent>
+                <S.BarVolumeImage>
+                  <S.BarVolumeImageSvg alt="volume">
+                    <use xlinkHref="img/icon/sprite.svg#icon-volume" />
+                  </S.BarVolumeImageSvg>
+                </S.BarVolumeImage>
+                <S.BarVolumeProgress className="_btn">
+                  <S.BarVolumeProgressLine
+                    className="_btn"
+                    type="range"
+                    name="range"
+                  />
+                </S.BarVolumeProgress>
+              </S.BarVolumeContent>
+            </S.BarVolumeBlock>
+          </S.BarPlayerBlock>
+        </S.BarContent>
+      </S.Bar>
+    </>
   );
 }
